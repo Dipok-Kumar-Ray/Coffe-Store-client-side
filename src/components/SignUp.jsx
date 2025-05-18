f3e38ee3e00f7eaf39cde159ef23d4d9f721cc53
Dipok Kumar Ray <dipok7316@gmail.com>
2025-05-19 04:12:58 +0600

    hopefully Coffee Store project complete

--- src/components/SignUp.jsx
+++ src/components/SignUp.jsx
@@ -10,9 +10,11 @@ const SignUp = () => {
         e.preventDefault();
         const form = e.target;
         const formData = new FormData(form);
-        const {email, password, ...userProfile} = Object.fromEntries(formData.entries());
+        const {email, password, ...restFormData} = Object.fromEntries(formData.entries());
 
-        console.log(email, password, userProfile);
+       
+
+        // console.log(email, password, userProfile);
         
         // const email = formData.get('email');
         // const password = formData.get('password');
@@ -22,6 +24,13 @@ const SignUp = () => {
         createUser(email, password)
         .then(result => {
             console.log(result.user);
+
+            const userProfile = {
+              email,
+              ...restFormData,
+              creationTime : result.user?.metadata?.creationTime,
+              lastSignInTime : result.user?.metadata?.lastSignInTime,
+            }
 
             //save profile info in the db
             fetch('http://localhost:3000/users', {
